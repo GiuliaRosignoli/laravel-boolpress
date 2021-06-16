5882c4f084f845e8b8592ff2ea4696e85dc98ef1
GiuliaRosignoli <giuliarosignoli@gmail.com>
2021-06-16 18:23:11 +0200

    Error

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/admin', 'HomeController@index')->name('home');
+
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->middleware('auth')
+    ->name('admin.')
+    ->group(function(){
+        Route::get('/', 'HomeController@index')->name('home');
+    });
