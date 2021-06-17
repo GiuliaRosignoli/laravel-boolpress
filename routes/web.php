361b907f57eee1da772edb8924bb947412a636c8
GiuliaRosignoli <giuliarosignoli@gmail.com>
2021-06-17 18:19:02 +0200

    index.blade added

--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,6 @@ Route::prefix('admin')
     ->name('admin.')
     ->group(function(){
         Route::get('/', 'HomeController@index')->name('home');
+
+        Route::resource('/posts', 'PostController');
     });
