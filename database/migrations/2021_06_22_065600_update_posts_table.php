f4414af608e0b8ff59cf839359ab62fc25cc5e9c
GiuliaRosignoli <giuliarosignoli@gmail.com>
2021-06-22 09:18:02 +0200

    Foreign key

---
+++ database/migrations/2021_06_22_065600_update_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {  
+        // Definition of a column
+        $table->unsignedBigInteger('category_id')->nullable()->after('slug');
+
+        // Foreign Key
+        $table->foreign('category_id')
+            ->references('id')
+            ->on('categories')
+            ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
+    }
+}
